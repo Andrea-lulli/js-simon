78ab3fb849e3bd7fea2409c475ca03d5c2bbb91d
Andrea-Lulli <andrea.lulli89@gmail.com>
2022-11-03 17:22:10 +0100

    logica esercizio completa

--- main.js
+++ main.js
@@ -12,15 +12,16 @@ let numeri = [];
 
 //funzione numeri random
 function numeriRandom(num) {
-  return Math.floor(Math.random() * num) + 1;  
+  return Math.floor(Math.random() * num) + 1;
+}
+let i
+//creazione di 5 numeri random dentro array 
+for (i = 0; i < 5; i++) {
+  let NumeriRandom = numeriRandom(50)
+  numeri.push(NumeriRandom)
+
 }
 
-//creazione di 5 numeri random dentro array 
-for (let i = 1; i <= 5; i++) { 
-  let NumeriRandom = numeriRandom(50)
-  numeri.push(NumeriRandom) 
-   
-}
 
 let Stampa = document.getElementById("stampa");
 Stampa.innerHTML += numeri
@@ -28,32 +29,76 @@ Stampa.innerHTML += numeri
 console.log(numeri);
 //funzione per far scomparire i numeri
 setTimeout(() => {
-  Stampa.classList.add("remove"); 
+  Stampa.classList.add("remove");
 }, 5000);
+
+//arroy dei numeri inseititi dall'utente
+let NumeriUtente = [];
+
 
 //funzione per far comparire 5 prompt
 setTimeout(() => {
   for (let y = 0; y < 5; y++) {
+
+
     let InserisciUtente = parseInt(prompt("Inseirisci Numero"));
-     NumeriUtente.push(InserisciUtente)}
+    NumeriUtente.push(InserisciUtente)
+  }
+
 }, 5030);
 
 
-//arroy dei numeri inseititi dall'utente
-let NumeriUtente = []
-console.log(NumeriUtente);
+//quali numeri ho indovinato
+let controllo = document.getElementById("stampacontrollo")
 
 
+//quantio numeri ho indovinato
+let stampaCalcolo = document.getElementById("stampacalcolo")
+let calcolo = 0
 
 
+// controllo dei numeri uguali tra quelli inseriti e quelli random
 setTimeout(() => {
-  if (numeri.includes(NumeriUtente)  ) {
-    console.log("preso");
-  }else{
-    console.log("no");
+  console.log(NumeriUtente);
+  controllo.innerHTML += `I numeri indovinati sono :`
+
+  if (NumeriUtente.includes(numeri[0])) {
+    console.log("preso")
+    controllo.innerHTML += ` ${numeri[0]}  `
+    calcolo++
+  } else {
+    console.log("sbagliato");
   }
-}, 15000);
+  if (NumeriUtente.includes(numeri[1])) {
+    console.log("preso")
+    controllo.innerHTML += ` ${numeri[1]} `
+    calcolo++
+  } else {
+    console.log("sbagliato");
+  }
+  if (NumeriUtente.includes(numeri[2])) {
+    console.log("preso")
+    controllo.innerHTML += ` ${numeri[2]} `
+    calcolo++
+  } else {
+    console.log("sbagliato");
+  }
+  if (NumeriUtente.includes(numeri[3])) {
+    console.log("preso")
+    controllo.innerHTML += ` ${numeri[3]} `
+    calcolo++
+  } else {
+    console.log("sbagliato");
+  }
+  if (NumeriUtente.includes(numeri[4])) {
+    console.log("preso")
+    controllo.innerHTML += ` ${numeri[4]} `
+    calcolo++
+  } else {
+    console.log("sbagliato");
+  }
+
+  stampaCalcolo.innerHTML = `Hai indovinato: ${calcolo} numeri`
+}, 10000)
 
 
-  
-  
